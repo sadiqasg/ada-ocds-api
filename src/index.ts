66d6039fffd1892d1e7ae80253bfe06cb50251aa
saslamp <sadiqasg@gmail.com>
2023-02-16 15:27:18 +0100

    modified cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import apiRoutes from "./api";
 import bodyParser from "body-parser";
@@ -8,7 +8,16 @@ import path from "path";
 
 const app = express();
 
-app.use(cors({credentials: true, origin: "*"}));
+// app.use(cors({credentials: true, origin: "*"}));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 admin.initializeApp();
 
@@ -16,17 +25,17 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-	if (process.env.NODE_ENV === "production") {
-		if (req.headers["x-forwarded-proto"] !== "https") {
-			return res.redirect("https://" + req.headers.host + req.url);
-		} else return next();
-	} else return next();
+    if (process.env.NODE_ENV === "production") {
+        if (req.headers["x-forwarded-proto"] !== "https") {
+            return res.redirect("https://" + req.headers.host + req.url);
+        } else return next();
+    } else return next();
 });
 
 app.use("/api", apiRoutes);
 
 app.get("/", (req: Request, res: Response) => {
-	res.status(200).send("Welcome to Adamawa OCDS API");
+    res.status(200).send("Welcome to Adamawa OCDS API");
 });
 
 // exports.app = functions.https.onRequest(app);
@@ -34,5 +43,5 @@ app.get("/", (req: Request, res: Response) => {
 const PORT = process.env.PORT || 3030;
 
 app.listen(PORT, () => {
-	console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
